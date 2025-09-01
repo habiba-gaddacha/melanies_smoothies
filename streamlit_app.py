63e88317e2dd83937b29693a240417d4e6bc8b35
habiba-gaddacha <77306054+habiba-gaddacha@users.noreply.github.com>
2025-09-01 04:18:16 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 
 from snowflake.snowpark.functions import col
 
@@ -40,6 +41,6 @@ if ingredients_list:
         st.success('Your Smoothie is ordered,' + name_on_order + '!', icon="✅")
 
 
-import requests
+
 
     
